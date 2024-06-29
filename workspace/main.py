8332f5cc57a15276de55e7a171b9d35d88b44ebd
Luis Rodrigo Barba Navarro <luisrodrigobarba.work@outlook.com>
2024-06-28 18:07:25 -0700

    Update: New Feature; Read Availables Routers, Establish Connection for Each Router

---
+++ workspace/main.py
@@ -0,0 +1,23 @@
+from modules.EstablishConnection import EstablishConnection
+from modules.ReadAvailableRouters import ReadAvailableRouters
+
+
+def main():
+    # Read available routers.
+    available_routers = ReadAvailableRouters()
+    available_routers.read()
+    router_list = available_routers.get_available_routers()
+
+    # Establish connection for each router.
+    for router in router_list:
+        try:
+            ssh = EstablishConnection().connect("routers/sections/" + router + ".json")
+            stdin, stdout, stderr = ssh.exec_command('ip address print')
+            print(stdout.read().decode())
+            ssh.close()
+        except Exception as e:
+            print(e)
+
+
+if __name__ == "__main__":
+    main()
