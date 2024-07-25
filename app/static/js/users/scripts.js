b40b4c7dbf6f4e47385b495eb1b03ed87b4ad015
Luis Rodrigo Barba Navarro <luisrodrigobarba.work@outlook.com>
2024-07-24 17:49:24 -0700

    Upgrade: Optimizing User Frontend; 'Delete Select', 'Delete All' Buttons Functions

--- app/static/js/users/scripts.js
+++ app/static/js/users/scripts.js
@@ -2,7 +2,8 @@
 $(document).ready(function () {
     $('#users-table').DataTable(
         {
-            responsive: true
+            "responsive": true,
+            "autoWidth": true
         }
     );
 });
@@ -22,4 +23,57 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     // Add event listener to the form to verify if the checkbox is checked
 });
+
 // Check if the checkbox is checked
+
+function deleteSelectedUsersModal(urlIn, urlOut) {
+    document.getElementById('delete-selected').addEventListener('click', function () {
+        const checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+        document.getElementById('total-selected').innerText = String(checkboxes.length);
+    });
+
+    document.getElementById('confirm-delete').addEventListener('click', function () {
+        const checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+        const selectedIds = Array.from(checkboxes).map(cb => cb.id.replace('user-', ''));
+
+        if (selectedIds.length > 0) {
+            fetch(urlIn, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({users_ids: selectedIds})
+                }
+            ).then(response => {
+                if (response.ok) {
+                    location.href = urlOut;
+                } else {
+                    alert('Failed to delete users');
+                }
+            });
+        } else {
+            alert('No users selected');
+        }
+    });
+}
+
+function deleteAllUsersModal(urlIn, urlOut) {
+    document.getElementById('confirm-delete-all').addEventListener('click', function () {
+        if (document.querySelectorAll('input[type="checkbox"]').length === 0) {
+            alert('No users to delete');
+        } else {
+            fetch(urlIn, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(response => {
+                if (response.ok) {
+                    location.href = urlOut;
+                } else {
+                    alert('Failed to delete users');
+                }
+            });
+        }
+    });
+}
