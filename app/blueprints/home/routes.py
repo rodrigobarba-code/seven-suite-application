3ff00efcc760547bb010670d61c4f036363b0fc5
Luis Rodrigo Barba Navarro <luisrodrigobarba.work@outlook.com>
2024-07-31 17:06:56 -0700

    Hot Fix: Blueprint/Home Commented and Optimized code

--- app/blueprints/home/routes.py
+++ app/blueprints/home/routes.py
@@ -1,14 +1,12 @@
 # Importing Required Libraries
 from . import home_bp
-from flask import render_template, redirect, url_for, flash, request, jsonify
+from flask import render_template
 from app.decorators import RequirementsDecorators as restriction
-
-
 # Importing Required Libraries
 
 # Home Main Route
 @home_bp.route('/', methods=['GET'])
 @restriction.login_required
 def home():
-    return render_template('home/home.html')
+    return render_template('home/home.html')  # Rendering Home Page
 # Home Main Route
