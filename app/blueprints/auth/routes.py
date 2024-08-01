5607085d4006685106fd61c7d1813791bb340726
Luis Rodrigo Barba Navarro <luisrodrigobarba.work@outlook.com>
2024-07-31 17:02:24 -0700

    Hot Fix: Blueprint/Auth Commented and Optimized code

--- app/blueprints/auth/routes.py
+++ app/blueprints/auth/routes.py
@@ -1,50 +1,61 @@
-from . import auth_bp
+# Importing necessary modules
 from flask import render_template, redirect, url_for, flash, request, session
-from app.blueprints.users.models import User
 import bcrypt
+# Importing necessary modules
+
+# Importing necessary decorators
 from app.decorators import RequirementsDecorators as restriction
+# Importing necessary decorators
 
+# Importing necessary models
+from app.blueprints.users.models import User
+# Importing necessary models
 
-# Login Route
+from . import auth_bp  # Importing the blueprint instance
+
+# Auth Login Route
 @auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password').encode('utf-8')  # Encode the password to bytes
+    if request.method == 'POST':  # If the request method is POST
+        username = request.form.get('username')  # Get the username from the form
+        password = request.form.get('password').encode('utf-8')  # Get the password from the form and encode it
+        # Query the database for the user to check if it exists
         user = User.query.filter_by(user_username=username).first()
-        if user:
-            print(f"User found: {user.user_username}")
-            if bcrypt.checkpw(password, user.user_password):  # Check the password hash
-                print("Password is correct")
-                session['user_id'] = user.user_id
-                session['user_privileges'] = user.user_privileges  # Ensure this line is present
-                session['user_name'] = user.user_name
-                session['user_lastname'] = user.user_lastname  # Add this line
-                if user.user_privileges == 'admin':
+        if user:  # If the user exists, will try to check the password, otherwise will return an error message
+            if bcrypt.checkpw(password, user.user_password):  # Check the password hash with the one in the database
+                # Create the session variables for the user
+                session['user_id'] = user.user_id  # Identify the user by the user_id
+                session['user_privileges'] = user.user_privileges  # Identify the user by the user_privileges
+                session['user_name'] = user.user_name  # Indentify the user by the user_name
+                session['user_lastname'] = user.user_lastname  # Indentify the user by the user_lastname
+                # Create the session variables for the user
+
+                # Set the user avatar based on the user privileges
+                if user.user_privileges == 'admin':  # If the user is an admin, will set the admin avatar
                     session['user_avatar'] = url_for('static', filename='img/user_avatars/admin_avatar.svg')
-                elif user.user_privileges == 'employee':
+                elif user.user_privileges == 'employee':  # If the user is an employee, will set the employee avatar
                     session['user_avatar'] = url_for('static', filename='img/user_avatars/user_avatar.svg')
                 elif user.user_privileges == 'guest':
                     session['user_avatar'] = url_for('static', filename='img/user_avatars/guest_avatar.svg')
                 return redirect(url_for('home.home'))
+                # Set the user avatar based on the user privileges
             else:
-                print("Password is incorrect")
-                flash('Invalid username or password')
+                flash('Invalid password, are you sure you typed it correctly?')  # If the password is incorrect, will return an error message
         else:
-            print("User not found")
-            flash('Invalid username or password')
-    return render_template('auth/login.html')
+            flash('Invalid username, maybe you need to register')  # If the user does not exist, will return an error message
+    return render_template('auth/login.html')  # If the request method is GET, will render the login page
+# Auth Login Route
 
-
-# Log out Route
+# Auth Logout Route
 @auth_bp.route('/logout')
 def logout():
-    session.pop('user_id', None)
-    return redirect(url_for('auth.login'))
+    # Remove all user information from client session
+    session.pop('user_id', None)  # Remove the user_id session variable
+    session.pop('user_privileges', None)  # Remove the user_privileges session variable
+    session.pop('user_name', None)  # Remove the user_name session variable
+    session.pop('user_lastname', None)  # Remove the user_lastname session variable
+    session.pop('user_avatar', None)  # Remove the user_avatar session variable
+    # Remove all user information from client session
 
-
-# Example of a protected route
-@auth_bp.route('/protected')
-@restriction.login_required
-def protected():
-    return 'This is a protected route'
+    return redirect(url_for('auth.login'))  # Redirect the user to the login page
+# Auth Logout Route
