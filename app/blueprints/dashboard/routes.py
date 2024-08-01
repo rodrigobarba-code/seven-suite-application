c05f9eac85f7511186bb8a139c85a40d819d9421
Luis Rodrigo Barba Navarro <luisrodrigobarba.work@outlook.com>
2024-07-31 17:06:01 -0700

    Hot Fix: Blueprint/Dashboard Commented and Optimized code

--- app/blueprints/dashboard/routes.py
+++ app/blueprints/dashboard/routes.py
@@ -1,14 +1,13 @@
 # Importing Required Libraries
 from . import dashboard_bp
-from flask import render_template, redirect, url_for, flash, request, jsonify
+from flask import render_template
 from app.decorators import RequirementsDecorators as restriction
-
-
 # Importing Required Libraries
 
 # Dashboard Main Route
 @dashboard_bp.route('/', methods=['GET'])
-@restriction.login_required
+@restriction.login_required  # Login Required Decorator
+@restriction.admin_required  # Admin Required Decorator
 def dashboard():
-    return render_template('dashboard/dashboard.html')
+    return render_template('dashboard/dashboard.html')  # Rendering Dashboard Template
 # Dashboard Main Route
